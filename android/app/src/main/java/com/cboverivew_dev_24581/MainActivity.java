32f9ddcc02c501c4c850560f17988bbb2306a881
Crowdbotics <engineering@crowdbotics.com>
2022-05-30 17:11:12 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cboverivew_dev_24581/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cboverivew_dev_24581;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cboverivew_dev_24581";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cboverivew_dev_24581",
+  url: "https://cboverivew_dev_24581.botics.co",
+  api: "https://cboverivew_dev_24581.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
