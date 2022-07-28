1b15d148f3a1307b99bca2edd3c16917d808a460
Sharaiz Ali <shahraiz.ali@crowdbotics.com>
2022-07-28 13:36:04 +0200

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled8/index.js
@@ -0,0 +1,92 @@
+import { Pressable } from "react-native";
+import { Text } from "react-native";
+import { TextInput } from "react-native";
+import React from "react";
+import { View } from "react-native";
+
+const Untitled8 = ({
+  navigation
+}) => {
+  return <View style={{
+    backgroundColor: '#f0f0f1',
+    padding: 10,
+    position: 'relative',
+    height: '100%'
+  }}>
+      <View style={{
+      left: 0,
+      top: 0,
+      position: "absolute",
+      height: 103,
+      width: 197,
+      backgroundColor: "#ffddf2",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View><View style={{
+      left: 66,
+      top: 59,
+      position: "absolute",
+      height: 60,
+      width: 60,
+      backgroundColor: "#aae9df",
+      borderRadius: "50%",
+      color: "#777777"
+    }}></View><TextInput style={{
+      left: 23,
+      top: 149,
+      position: "absolute",
+      backgroundColor: "#ffffff",
+      borderColor: "#cccccc",
+      width: 150,
+      height: 30
+    }} placeholder="John Doe"></TextInput><Text style={{
+      left: 23,
+      top: 128,
+      position: "absolute",
+      width: 100,
+      height: 50,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}>Name
+    </Text><Text style={{
+      left: 23,
+      top: 188,
+      position: "absolute",
+      width: 100,
+      height: 50,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}>DOB</Text><TextInput style={{
+      left: 23,
+      top: 209,
+      position: "absolute",
+      backgroundColor: "#ffffff",
+      borderColor: "#cccccc",
+      width: 150,
+      height: 30
+    }} placeholder="YYYY-MM-DD"></TextInput><Pressable onPress={() => navigation.navigate("Untitled9")}><View style={{
+        left: 29,
+        top: 303,
+        position: "absolute",
+        height: 25,
+        width: 140,
+        backgroundColor: "#000000",
+        borderRadius: 8,
+        color: "#777777"
+      }}></View></Pressable><Text style={{
+      position: "absolute",
+      left: 36,
+      top: 309,
+      width: 122,
+      height: 24,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center",
+      color: "#ffffff"
+    }}>Save</Text></View>;
+};
+
+export default Untitled8;
---
+++ screens/Untitled9/index.js
@@ -0,0 +1,34 @@
+import { Text } from "react-native";
+import React from "react";
+import { View } from "react-native";
+
+const Untitled9 = () => {
+  return <View style={{
+    backgroundColor: '#f0f0f1',
+    padding: 10,
+    position: 'relative',
+    height: '100%'
+  }}>
+      <View style={{
+      left: 38,
+      top: 104,
+      position: "absolute",
+      width: 124,
+      height: 124,
+      backgroundColor: "#e8ffcf",
+      borderRadius: "50%"
+    }}></View><Text style={{
+      left: 51,
+      top: 139,
+      position: "absolute",
+      width: 100,
+      height: 71,
+      lineHeight: 29,
+      fontSize: 25,
+      borderRadius: 0,
+      textAlign: "center",
+      fontWeight: 700
+    }}>Thank You</Text></View>;
+};
+
+export default Untitled9;
